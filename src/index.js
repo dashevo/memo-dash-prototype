1673bcfeea849c89dfce9c8ba04939dd45f8414e
davidka <davidka.h@gmail.com>
2018-07-12 15:14:38 +0200

    Add hot reload for app

--- src/index.js
+++ src/index.js
@@ -6,17 +6,31 @@ import registerServiceWorker from './registerServiceWorker'
 import { createStore, history } from './store'
 import App from './app'
 import { ConnectedRouter } from 'connected-react-router'
+import { AppContainer } from 'react-hot-loader'
 
 const store = createStore()
 
 console.log('CREATE_STORE', store.getState())
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
-)
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
 registerServiceWorker()
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    render(App)
+  })
+}
