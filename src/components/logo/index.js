0e1310b32ac5a58383b5104dd1dfc6ca04dc78e0
David Hahn <davidka.h@gmail.com>
2018-07-31 12:46:57 +0200

    Change logo to a functional component

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from './images/logo.svg'
 import './styles.css'
 
-class Logo extends Component {
-  render() {
-    return (
-      <span className="logo">
-        <img src={logo} className="logo__image" alt="logo" />
-      </span>
-    )
-  }
+const Logo = () => {
+  return (
+    <span className="logo">
+      <img src={logo} className="logo__image" alt="logo" />
+    </span>
+  )
 }
 
 export default Logo
