1cc76a4a1634478d63d9fb369285a263e5fe2ce4
David Hahn <davidka.h@gmail.com>
2018-08-11 16:43:04 +0200

    Add ability to post a memo

--- src/scenes/home-view/home-view.component.jsx
+++ src/scenes/home-view/home-view.component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
 import './styles.css'
 import MemosContainer from '../../components/memo/memos.container'
+import PostMemoContainer from '../../components/memo/post-memo/post-memo.container'
 
 export class HomeViewComponent extends Component {
   componentDidMount() {
@@ -12,6 +13,7 @@ export class HomeViewComponent extends Component {
     return (
       <React.Fragment>
         <Container text style={{ marginTop: '7em' }}>
+          <PostMemoContainer />
           <MemosContainer memos={this.props.memos} />
         </Container>
       </React.Fragment>
