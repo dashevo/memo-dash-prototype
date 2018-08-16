677a68570371d7fca26853395f4ec0e0a5e6e37d
David Hahn <davidka.h@gmail.com>
2018-08-16 17:00:16 +0200

    Add component to render a list of user profiles

---
+++ src/components/user-profile/user-profiles.component.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import UserProfilesComponent from './user-profiles.component'
+import testUsers from '../../test-utils/test-users'
+
+describe('<UserProfilesComponent />', () => {
+  let wrapper
+  let userProfiles
+
+  beforeEach(() => {
+    userProfiles = Object.values(testUsers).map(testUser => testUser.profile)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+  })
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<UserProfilesComponent userProfiles={[]} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render memos', () => {
+    wrapper = shallow(<UserProfilesComponent userProfiles={userProfiles} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
