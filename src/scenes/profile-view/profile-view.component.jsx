8e5888912740e0ac9161af86d22afdf265fd5a8b
David Hahn <davidka.h@gmail.com>
2018-08-18 14:28:01 +0200

    Extend profile-view with a tabbar to display followers/following

--- src/scenes/profile-view/profile-view.component.jsx
+++ src/scenes/profile-view/profile-view.component.jsx
@@ -1,23 +1,108 @@
 import React, { Component } from 'react'
-import { Dimmer, Loader, Container, Image, Grid, Menu, Segment, Header } from 'semantic-ui-react'
+import { Dimmer, Loader, Container, Image, Grid, Segment, Header, Tab } from 'semantic-ui-react'
 import MemosContainer from '../../components/memo/memos.container'
+import UserProfilesComponent from '../../components/user-profile/user-profiles.component'
+
+const TABS = {
+  MEMOS: 0,
+  FOLLOWERS: 1,
+  FOLLOWING: 2
+}
+
+const PATHS = {
+  MEMOS: 'memos',
+  FOLLOWERS: 'followers',
+  FOLLOWING: 'following'
+}
+
+const mapPathnameToTab = pathname => {
+  switch (pathname) {
+    case PATHS.MEMOS:
+      return TABS.MEMOS
+    case PATHS.FOLLOWERS:
+      return TABS.FOLLOWERS
+    case PATHS.FOLLOWING:
+      return TABS.FOLLOWING
+    default:
+      return TABS.MEMOS
+  }
+}
 
 export class ProfileViewComponent extends Component {
   componentDidMount() {
-    const {
-      match: {
-        params: { username }
-      },
-      memos,
-      profile
-    } = this.props
+    const { username, pathname, profile } = this.props
 
     if (!profile) this.props.getUser(username)
-    if (!memos) this.props.getMemosForUser(username)
+    this.onTabChange(undefined, { activeIndex: mapPathnameToTab(pathname) })
   }
 
+  componentDidUpdate(prevProps) {
+    const { username, pathname } = this.props
+    const { username: prevUsername } = prevProps
+
+    if (username !== prevUsername) {
+      this.onTabChange(undefined, { activeIndex: mapPathnameToTab(pathname) })
+    }
+  }
+
+  onTabChange = (event, data) => {
+    const {
+      memos,
+      followers,
+      following,
+      username,
+      onMemosClicked,
+      onFollowersClicked,
+      onFollowingClicked
+    } = this.props
+
+    switch (data.activeIndex) {
+      case TABS.MEMOS:
+        onMemosClicked(memos, username)
+        break
+      case TABS.FOLLOWERS:
+        onFollowersClicked(followers, username)
+        break
+      case TABS.FOLLOWING:
+        onFollowingClicked(following, username)
+        break
+      default:
+        break
+    }
+  }
+
+  renderMemos = memos => (
+    <Tab.Pane loading={!memos}>
+      <Container>
+        <MemosContainer memos={memos} />
+      </Container>
+    </Tab.Pane>
+  )
+
+  renderFollowers = followers => (
+    <Tab.Pane loading={!followers}>
+      {followers ? (
+        <UserProfilesComponent
+          actualUsername={this.props.username}
+          userProfiles={followers.map(user => user.profile)}
+        />
+      ) : null}
+    </Tab.Pane>
+  )
+
+  renderFollowing = following => (
+    <Tab.Pane loading={!following}>
+      {following ? (
+        <UserProfilesComponent
+          actualUsername={this.props.username}
+          userProfiles={following.map(user => user.profile)}
+        />
+      ) : null}
+    </Tab.Pane>
+  )
+
   render() {
-    const { profile, memos } = this.props
+    const { profile, memos, followers, following, pathname } = this.props
 
     return (
       <React.Fragment>
@@ -35,16 +120,26 @@ export class ProfileViewComponent extends Component {
                   <Segment>{profile.bio}</Segment>
                 </Grid.Column>
                 <Grid.Column width={12}>
-                  <Menu>
-                    <Menu.Item active as="a">
-                      Memos ({memos ? memos.length : 0})
-                    </Menu.Item>
-                    <Menu.Item as="a">Followers ({profile.followersCount})</Menu.Item>
-                    <Menu.Item as="a">Following ({profile.followingCount})</Menu.Item>
-                  </Menu>
-                  <Container>
-                    <MemosContainer memos={memos} />
-                  </Container>
+                  <Tab
+                    onTabChange={this.onTabChange}
+                    panes={[
+                      {
+                        active: pathname === PATHS.MEMOS,
+                        menuItem: `Memos${memos ? ` (${memos.length})` : ''}`,
+                        render: () => this.renderMemos(memos)
+                      },
+                      {
+                        active: pathname === PATHS.FOLLOWERS,
+                        menuItem: `Followers (${profile.followersCount})`,
+                        render: () => this.renderFollowers(followers)
+                      },
+                      {
+                        active: pathname === PATHS.FOLLOWING,
+                        menuItem: `Following (${profile.followingCount})`,
+                        render: () => this.renderFollowing(following)
+                      }
+                    ]}
+                  />
                 </Grid.Column>
               </Grid.Row>
             </Grid>
--- src/scenes/profile-view/profile-view.container.test.jsx
+++ src/scenes/profile-view/profile-view.container.test.jsx
@@ -2,41 +2,55 @@ import React from 'react'
 import configureStore from 'redux-mock-store'
 import { shallow } from 'enzyme'
 import ProfileViewContainer from './profile-view.container'
+import testUsers from '../../test-utils/test-users'
 
 describe('<ProfileViewContainer />', () => {
   let store
   let ownProps
 
-  beforeEach(() => {
-    // Mock store
-    const mockStore = configureStore()
-    store = mockStore({
+  const alice = testUsers['alice']
+  const bob = testUsers['bob']
+
+  const makeMockStore = (currentUser, profileOfUser) =>
+    configureStore()({
       user: {
-        currentUser: 'User1',
-        users: [
-          {
-            username: 'User1',
-            profile: {
-              avatarUrl: 'AvatarUrl',
-              username: 'Username',
-              bio: 'Bio',
-              followersCount: 1,
-              followingCount: 1
-            },
-            memos: [{ message: 'message', createdAt: 'createdAt' }]
-          }
-        ]
+        currentUser,
+        users: {
+          [alice.username]: alice,
+          [bob.username]: bob
+        }
+      },
+      router: {
+        location: {
+          pathname: `/profile/${profileOfUser}/memos`
+        }
       }
     })
 
-    ownProps = { match: { params: { username: 'username' } } }
+  const makeOwnProps = username => ({
+    match: {
+      params: {
+        username
+      }
+    }
+  })
 
+  beforeEach(() => {
     const div = document.createElement('div')
     document.body.appendChild(div)
   })
 
   describe('Shallow rendering', () => {
     it('renders without crashing', () => {
+      ownProps = makeOwnProps(alice.username)
+      store = makeMockStore(alice.username, alice.username)
+      const wrapper = shallow(<ProfileViewContainer {...ownProps} />, { context: { store } })
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('should render user prfoile that different from current user', () => {
+      ownProps = makeOwnProps(bob.username)
+      store = makeMockStore(alice.username, bob.username)
       const wrapper = shallow(<ProfileViewContainer {...ownProps} />, { context: { store } })
       expect(wrapper).toMatchSnapshot()
     })
