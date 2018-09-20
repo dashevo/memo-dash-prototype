f75cc5a9ba9a2c307fcfdb15a14235bf6b47fbdb
davidka <davidka.h@gmail.com>
2018-09-20 14:50:46 +0200

    Route to home when clicked on logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,18 +4,20 @@ import Logo from '../logo/logo.component'
 import UserMenuContainer from './user-menu/user-menu.container'
 
 const HeaderComponent = props => {
+  const { onHomeClicked, onUsersClicked } = props
+
   return (
     <header>
       <Menu fixed="top" color="blue">
         <Container>
-          <Menu.Item as="a" header>
+          <Menu.Item as="a" onClick={onHomeClicked} header>
             <Logo />
             MemoDash
           </Menu.Item>
-          <Menu.Item as="a" onClick={props.onHomeClicked} active={props.location === '/home'}>
+          <Menu.Item as="a" onClick={onHomeClicked} active={props.location === '/home'}>
             Home
           </Menu.Item>
-          <Menu.Item as="a" onClick={props.onUsersClicked} active={props.location === '/users'}>
+          <Menu.Item as="a" onClick={onUsersClicked} active={props.location === '/users'}>
             Users
           </Menu.Item>
           <Menu.Menu position="right">
