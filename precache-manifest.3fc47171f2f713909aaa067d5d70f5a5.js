69bed74205072e637a16899e9174f3ad8d833dea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-28 13:00:50 +0000

    Deploy dashevo/memo-dash to github.com/dashevo/memo-dash.git:gh-pages

---
+++ precache-manifest.3fc47171f2f713909aaa067d5d70f5a5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/memo-dash/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "9553a93b94002ca6981e",
+    "url": "/memo-dash/static/css/main.4adb2b43.chunk.css"
+  },
+  {
+    "revision": "7ce217af1bd779026c5b",
+    "url": "/memo-dash/static/js/1.7ce217af.chunk.js"
+  },
+  {
+    "revision": "d0d471d994bbad6af66d",
+    "url": "/memo-dash/static/js/runtime~main.d0d471d9.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/memo-dash/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/memo-dash/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/memo-dash/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/memo-dash/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/memo-dash/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/memo-dash/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "9553a93b94002ca6981e",
+    "url": "/memo-dash/static/js/main.9553a93b.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/memo-dash/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/memo-dash/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/memo-dash/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/memo-dash/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/memo-dash/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/memo-dash/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/memo-dash/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/memo-dash/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/memo-dash/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "6980b57af4d587af4e632ddd899d07f5",
+    "url": "/memo-dash/index.html"
+  }
+];
